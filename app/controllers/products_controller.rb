3b26156c4838b02df0c0dbfc52a837b22c370955
Nicholas Silas <njsilas@gmail.com>
2021-01-14 17:18:28 -0500

    commit tree 330aa8708a0ac101528049fadd679e31226d52da

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,9 @@
+class ProductsController < ApplicationController
+  def index
+  @products = Product.all
+    end
+    def add
+        cart << params[:product]
+        render :index
+    end
+end     
